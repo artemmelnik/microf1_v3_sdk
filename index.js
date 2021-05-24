3c4971c2ae3a550055b9c7d41e7997997b4c05ad
Artem Melnik <artemmelnik989@gmail.com>
2021-05-24 14:21:47 +0300

    ss

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-module.exports = function tiny(string) {
-    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-    return string.replace(/\s/g, "");
-};
+import axios from 'axios';
+
+class MicrofSDK {
+    constructor(token, baseUrl) {
+        axios.defaults.headers.common.Accept = 'application/json';
+        axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+        if (token) {
+            axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+        }
+
+        axios.defaults.headers.common['MF-UNIT-ID'] = 1;
+
+        this.axios = axios.create({
+            baseURL: baseUrl,
+            responseType: 'json',
+        });
+    }
+
+    getAxios() {
+        return this.axios;
+    }
+
+    getTestString() {
+        return 'Test String';
+    }
+}
+
+module.exports.MicrofSDK = MicrofSDK;
