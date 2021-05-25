b4a394ff2ac2ba5408147333eede5180467adf8d
Artem Melnik <artemmelnik989@gmail.com>
2021-05-25 10:51:49 +0300

    s

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-class MicrofSDK {
-    constructor(token, baseUrl) {
+class BorrowerSDK {
+    constructor(token, baseUrl, unitId) {
         axios.defaults.headers.common.Accept = 'application/json';
         axios.defaults.headers.common['Content-Type'] = 'application/json';
 
@@ -9,7 +9,7 @@ class MicrofSDK {
             axios.defaults.headers.common.Authorization = `Bearer ${token}`;
         }
 
-        axios.defaults.headers.common['MF-UNIT-ID'] = 1;
+        axios.defaults.headers.common['MF-UNIT-ID'] = unitId;
 
         this.axios = axios.create({
             baseURL: baseUrl,
@@ -21,9 +21,21 @@ class MicrofSDK {
         return this.axios;
     }
 
-    getTestString() {
-        return 'Test String';
+    getDeposit(params) {
+        return this.getAxios().post('/deposit/borrower/deposit/get', params);
+    }
+
+    refillDeposit(params) {
+        return this.getAxios().post('/deposit/borrower/deposit/refill-deposit', params);
+    }
+
+    getDepositPayoutSchedules() {
+        return this.getAxios().post('/deposit/borrower/deposit-payout-schedule/list');
+    }
+
+    addDepositPayoutSchedules(params) {
+        return this.getAxios().post('/deposit/borrower/deposit-payout-schedule/add', params);
     }
 }
 
-module.exports.MicrofSDK = MicrofSDK;
+export {BorrowerSDK};
