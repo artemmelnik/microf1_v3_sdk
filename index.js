c8845486aeee89da6123bbd6013483e7bdbbe9b2
Artem Melnik <artemmelnik989@gmail.com>
2021-06-01 10:53:05 +0300

    s

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 import axios from 'axios';
 
+const ResponseHandler = {
+    dispatch(response) {
+        if (response[0].code === 0) {
+            return {
+                result: response[0],
+                data: response[0].data,
+                status: true,
+            };
+        } else {
+            return {
+                data: response,
+                status: false,
+            };
+        }
+    },
+};
+
 class ManagerSDK {
-    constructor(token, baseUrl, unitId) {}
+    constructor(token, baseUrl, unitId) {
+        axios.defaults.headers.common.Accept = 'application/json';
+        axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+        if (token) {
+            axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+            axios.defaults.headers.common['MF-UNIT-ID'] = unitId;
+        }
+
+        this.axios = axios.create({
+            baseURL: baseUrl,
+            responseType: 'json',
+        });
+    }
+
+    getAxios() {
+        return this.axios;
+    }
+
+    approveDepositPayoutSchedule(params) {
+        return this.getAxios().post('/deposit/manager/deposit-payout-schedule/approve', params);
+    }
 }
 
 class BorrowerSDK {
@@ -29,6 +67,10 @@ class BorrowerSDK {
         return this.getAxios().post('borrower-credit-card/list');
     }
 
+    getDepositProducts() {
+        return this.getAxios().post('/deposit/borrower/deposit-product/list');
+    }
+
     getDeposit(params) {
         return this.getAxios().post('/deposit/borrower/deposit/get', params);
     }
@@ -49,4 +91,5 @@ class BorrowerSDK {
 export {
     BorrowerSDK,
     ManagerSDK,
+    ResponseHandler,
 };
